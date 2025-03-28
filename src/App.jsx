a44af6e826209b3d53b33cbee458ce1e3badf1de
IT22094704 <it22094704@my.sliit.lk>
2025-03-28 22:09:49 +0530

    fix: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,6 @@ import {Routes, Route} from 'react-router-dom'
 //Common
 import Home from './pages/Home.jsx';
 
-//Product
-import IndexProduct from './pages/products/IndexProduct.jsx';
-import CreateProduct from './pages/products/CreateProduct.jsx';
-import ViewProduct from './pages/products/ViewProduct.jsx';
-import EditProduct from './pages/products/EditProduct.jsx';
-import DeleteProduct from './pages/products/DeleteProduct.jsx';
-
 //Auth
 import Login from './pages/auth/Login.jsx';
 import Register from './pages/auth/Register.jsx';
@@ -19,24 +12,14 @@ import Profile from './pages/auth/Profile.jsx';
 import AuthSuccess from './pages/auth/AuthSuccess.jsx';
 import EditProfile from './pages/auth/EditProfile.jsx';
 
-//Isuru
-
-//Gihan
-
-//Dimuthu
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      {/** Product */}
-      <Route path="/product" element={<IndexProduct />} />
-      <Route path="/product/create" element={<CreateProduct />} />
-      <Route path="/product/detail/:id" element={<ViewProduct />} />
-      <Route path="/product/edit/:id" element={<EditProduct />} />
-      <Route path="/product/delete/:id" element={<DeleteProduct />} />
-
       {/** Sageevan */}
+      {/** Common */}
+      <Route path="/" element={<Home />} />
+
       {/** Auth */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
@@ -44,13 +27,6 @@ const App = () => {
       <Route path="/profile/edit" element={<EditProfile />} />
       <Route path="/auth-success" element={<AuthSuccess />} />
 
-      {/** Isuru */}
-
-      {/** Gihan */}
-
-      {/** Dimuthu */}
-
-      
     </Routes>
   )
 }
