ff680a25f900e2c5839396738226864f93d09971
IT22094704 <it22094704@my.sliit.lk>
2025-03-28 21:03:07 +0530

    Fix: Edit Profile Feat: Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 
+//Sageevan
 //Common
 import Home from './pages/Home.jsx';
 
@@ -16,6 +17,13 @@ import Login from './pages/auth/Login.jsx';
 import Register from './pages/auth/Register.jsx';
 import Profile from './pages/auth/Profile.jsx';
 import AuthSuccess from './pages/auth/AuthSuccess.jsx';
+import EditProfile from './pages/auth/EditProfile.jsx';
+
+//Isuru
+
+//Gihan
+
+//Dimuthu
 
 const App = () => {
   return (
@@ -28,11 +36,20 @@ const App = () => {
       <Route path="/product/edit/:id" element={<EditProduct />} />
       <Route path="/product/delete/:id" element={<DeleteProduct />} />
 
+      {/** Sageevan */}
       {/** Auth */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/profile/edit" element={<EditProfile />} />
       <Route path="/auth-success" element={<AuthSuccess />} />
+
+      {/** Isuru */}
+
+      {/** Gihan */}
+
+      {/** Dimuthu */}
+
       
     </Routes>
   )
